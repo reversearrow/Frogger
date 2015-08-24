33574d18ccbc9781b8160b6c6738dc6304eaa8ce
reversearrow <reversearrow@gmail.com>
2015-08-24 13:09:49 -0400

    Fixed the Semicolon Errors

--- js/app.js
+++ js/app.js
@@ -9,17 +9,17 @@ var Game= function(image,x,y,width,height){
 	/* This is a game superclass. This contains methods for assigninng image to object,
 	* position x and y to the object and defining their width and height.
 	*/
-	this.sprite= "images/".concat(image)
+	this.sprite= "images/".concat(image);
 	this.x= x;
 	this.y= y;
 	this.width= width;
 	this.height= height;
-}
+};
 
 //Render method is added to the prototype object of the Game.
 Game.prototype.render= function(){
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Enemies our player must avoid
 var Enemy= function(image,x,y,width,height){
@@ -30,13 +30,13 @@ var Enemy= function(image,x,y,width,height){
 	*/
 	Game.call(this,image,x,y,width,height);
 	this.speed= Math.floor((Math.random() * 5) + 1);
-}
+};
 
 //Creating a prototype chain and delegating failed look ups to Game.prototype.
 Enemy.prototype= Object.create(Game.prototype);
 
 //Making sure .constructor property of all the objects instantiated from the Enemy class points to the enemy class as its constructor.
-Enemy.prototype.constructor= Enemy
+Enemy.prototype.constructor= Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -54,13 +54,13 @@ Enemy.prototype.update= function(dt){
 	//Also it will reduce one life from the player's total lives.
 	//It checks for the players lives, if lives is 0 then it will display Game Over.
 	if (player.collide(this,this.width,this.height)){
-		player.resetgame()
+		player.resetgame();
 		player.lives -= 1;
 		if (player.lives=== 0){
-			console.log("Game Over")
+			console.log("Game Over");
 		}
-	};
-}
+	}
+};
 
 
 // Now write your own player class
@@ -90,7 +90,7 @@ var Player= function(image,x,y,width,height){
 	Game.call(this,image,x,y,width,height);
 	this.score= 0;
 	this.lives= 5;
-}
+};
 
 //Creating a prototype chain and delegating failed lookup methods to the Game.prototype.
 Player.prototype= Object.create(Game.prototype);
@@ -103,7 +103,7 @@ Player.prototype.update= function(){
 	if(player.y < 30){
 		player.resetgame();
 	}
-}
+};
 
 // This method is to handle incoming keyboard input. Based on the input value it will increase or reduce the x or y values.
 Player.prototype.handleInput= function(key){
@@ -126,15 +126,15 @@ Player.prototype.handleInput= function(key){
 Player.prototype.collide= function(object,object_width,object_height){
 	if(object.x < (this.x + this.width) && (object.x + object_width) > this.x &&
 		object.y < (this.y + this.height) && (object_height + object.y) > this.y){
-	return true
+	return true;
 	}
-}
+};
 
 //This method will reset the position of the player. This method will put the player to it's starting position.
 Player.prototype.resetgame= function (){
 	player.x= start_x;
 	player.y= start_y;
-}
+};
 
 //This method will reset the player objects starting postion as well as its score and lives.
 Player.prototype.reset= function (){
@@ -142,7 +142,7 @@ Player.prototype.reset= function (){
 	player.y= start_y;
 	this.score= 0;
 	this.lives= 5;
-}
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -160,11 +160,10 @@ document.addEventListener('keyup', function(e){
 for (i=0; i<4; i++){
 	x= Math.floor(Math.random() * (-200 - 70 + 1)) - 70;
 	y= Math.floor(Math.random() * (320 - 70 + 1)) + 70;
-	allEnemies.push(new Enemy("enemy-bug.png",x,y,50,50))
+	allEnemies.push(new Enemy("enemy-bug.png",x,y,50,50));
 }
 //Instantiating the Player object
 var player= new Player("char-boy.png",start_x,start_y,player_width,player_height);
-
 
 
 
